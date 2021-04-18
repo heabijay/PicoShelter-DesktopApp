f16f8822c993770d79b38b9baa4c37c5b3457113
heabijay <heabijay@gmail.com>
2021-04-18 04:52:07 +0300

    Start page design

---
+++ PicoShelter-DesktopApp/ApplicationViewModel.cs
@@ -0,0 +1,34 @@
+﻿using PicoShelter_DesktopApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PicoShelter_DesktopApp
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+        public ApplicationViewModel()
+        {
+            currentPage = new WelcomePage();
+        }
+
+        private Page currentPage { get; set; }
+        public Page CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
